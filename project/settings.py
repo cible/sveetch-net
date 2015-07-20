cd4a242ede332ed5cf4db412073fc481be9ae145
Cédric De Wilde <c.dewilde@gmail.com>
2015-07-20 13:17:52 +0200

    Update settings to new format

--- project/settings.py
+++ project/settings.py
@@ -156,6 +156,10 @@ INSTALLED_APPS = (
     'django.contrib.sites',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django_comments',
+    'mptt',
+    'tagging',
+    'zinnia',
     # admin
     'django.contrib.admin',
     'django.contrib.admindocs',
@@ -215,13 +219,42 @@ ALLOWED_HOSTS = ['*'] # FIXME Put here the domain names
 
 
 TEMPLATE_CONTEXT_PROCESSORS = (
-    'django.contrib.auth.context_processors.auth',
-    'django.core.context_processors.debug',
-    'django.core.context_processors.i18n',
-    'django.core.context_processors.media',
-    'django.core.context_processors.static',
-    'django.core.context_processors.tz',
-    'django.contrib.messages.context_processors.messages')
+    "django.contrib.auth.context_processors.auth",
+    "django.template.context_processors.debug",
+    "django.template.context_processors.i18n",
+    "django.template.context_processors.media",
+    "django.template.context_processors.static",
+    "django.template.context_processors.tz",
+    'django.core.context_processors.request',
+    "django.contrib.messages.context_processors.messages"
+)
+
+DOCUMENTS_PAGE_TEMPLATE_DEFAULT = 'default'
+DOCUMENTS_PAGE_RESTRICTED = False
+
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [
+            PROJECT_PATH + '/templates/'
+        ],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                # Insert your TEMPLATE_CONTEXT_PROCESSORS here or use this
+                # list if you haven't customized them:
+                'django.contrib.auth.context_processors.auth',
+                'django.template.context_processors.debug',
+                'django.template.context_processors.i18n',
+                'django.template.context_processors.media',
+                'django.template.context_processors.static',
+                'django.template.context_processors.tz',
+                'django.template.context_processors.request',
+                'django.contrib.messages.context_processors.messages',
+            ],
+        },
+    },
+]
 
 
 #
