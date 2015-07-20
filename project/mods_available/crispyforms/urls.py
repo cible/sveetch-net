a9c4f2bc024e3fb7d834954fc1252163efaac0e3
Cédric De Wilde <c.dewilde@gmail.com>
2015-07-20 13:20:57 +0200

    I have no idea what i'm doing

--- project/mods_available/crispyforms/urls.py
+++ project/mods_available/crispyforms/urls.py
@@ -2,6 +2,6 @@
 """
 Map your djangocodemirror urls
 """
-urlpatterns = patterns('',
-    (r'^crispy-forms-foundation/', include('crispy_forms_foundation.urls')),
-) + urlpatterns
+#urlpatterns = patterns('',
+#    (r'^crispy-forms-foundation/', include('crispy_forms_foundation.urls')),
+#) + urlpatterns
